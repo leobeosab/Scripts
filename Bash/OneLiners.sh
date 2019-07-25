a63eae29229cbd1bd2110d40b48aec5a84ae4118
Ryan Wise <ryan@ryanwise.me>
2019-07-25 01:43:38 -0400

    Updated info

--- Bash/OneLiners.sh
+++ Bash/OneLiners.sh
@@ -1,3 +1,7 @@
+
+# Fuzz + Clone public http directories on a site
+# SITE = host with protocol ie http://10.10.10.140
+# STATUS = directory get response status code
 SITE="http://10.10.10.140" &&
   STATUS="301" &&
   gobuster dir -w /usr/share/SecLists-master/Discovery/Web-Content/big.txt -e -u $SITE |
